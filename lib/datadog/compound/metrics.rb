6103e352803a95c19c7229a944d682f5e0a6a6d1
Karol Galanciak <karol.galanciak@gmail.com>
2023-04-05 14:52:57 +0200

    initial commit

---
+++ lib/datadog/compound/metrics.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "metrics/version"
+
+module Datadog
+  module Compound
+    module Metrics
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
